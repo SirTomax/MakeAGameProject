d782c3132d7416750aa59ab955036e3c5ac16130
SirTomax <46323295+SirTomax@users.noreply.github.com>
2019-01-15 21:28:02 +0100

    Implemented fliping player character and only jumping from ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public SpriteRenderer sprite;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    private bool groundCollision;
+
     void Start()
     {
 
+    }
+
+    void FixedUpdate()
+    {
+        groundCollision = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     void Update()
@@ -16,13 +27,15 @@ public class PlayerController : MonoBehaviour
         var transform = GetComponent<Transform>();
         if (Input.GetKey("right"))
         {
+            sprite.flipX = false;
             rigidBody.velocity = new Vector2(5, rigidBody.velocity.y);
         }
         if (Input.GetKey("left"))
         {
+            sprite.flipX = true;
             rigidBody.velocity = new Vector2(-5, rigidBody.velocity.y);
         }
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && groundCollision)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, 10);
         }
@@ -36,7 +49,7 @@ public class PlayerController : MonoBehaviour
     {
         if (other.name == "EnemyDamage")
         {
-            transform.position = new Vector2(transform.position - 2, -2);
+            transform.position = new Vector2(transform.position.x - 5, -2);
         }
     }
 }
